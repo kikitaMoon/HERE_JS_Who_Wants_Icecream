c2bff6b56cde5d635067f57306a9682ec1e4be80
kikita <kikitaMoon@gmail.com>
2019-01-03 16:20:07 +0800

    Part4_Advanced_Routing
    Part4_Advanced_Routing

---
+++ Part4_Advanced_Routing/scripts/app.js
@@ -0,0 +1,26 @@
+// Get Map Container Element
+var mapContainer = document.getElementById('map-container');
+
+
+// ----------------------------------------------------------------------
+// instantiate the Platform class, and specify the APPID & APPCODE
+var platform = new H.service.Platform({
+  app_id: 'oyNWTbKIov6XPMlrXxCH', // // <-- ENTER YOUR APP ID HERE
+  app_code: '_W_7qfgpOxthQ4tQQKkbLA', // <-- ENTER YOUR APP CODE HERE
+});
+
+// ----------------------------------------------------------------------
+// mapOptions
+var endCoordinates = {
+  lat: 39.997117, // Yiheyuan Park in Beijing, China
+  lng: 116.268112
+};
+var mapOptions = {
+  center: endCoordinates,
+  zoom: 14
+}
+
+// only a single line after code cleaning up (Pre-session: Refactor)
+var map = new HEREMap(mapContainer, platform, mapOptions);
+
+
