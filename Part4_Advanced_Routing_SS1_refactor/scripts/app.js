3dcf652c8a4a1d8ec271cae132ce51f652d218d1
kikita <kikitaMoon@gmail.com>
2019-01-03 17:46:25 +0800

    Part4_Advanced_Routing_SS1_refactor
    Part4_Advanced_Routing_SS1_refactor

--- Part4_Advanced_Routing_SS1_refactor/scripts/app.js
+++ Part4_Advanced_Routing_SS1_refactor/scripts/app.js
@@ -5,8 +5,8 @@ var mapContainer = document.getElementById('map-container');
 // ----------------------------------------------------------------------
 // instantiate the Platform class, and specify the APPID & APPCODE
 var platform = new H.service.Platform({
-  app_id: 'oyNWTbKIov6XPMlrXxCH', // // <-- ENTER YOUR APP ID HERE
-  app_code: '_W_7qfgpOxthQ4tQQKkbLA', // <-- ENTER YOUR APP CODE HERE
+  app_id: 'oyNWTbKIov6XPMlrX***', // // <-- ENTER YOUR APP ID HERE
+  app_code: '_W_7qfgpOxthQ4tQQKk***', // <-- ENTER YOUR APP CODE HERE
 });
 
 // ----------------------------------------------------------------------
