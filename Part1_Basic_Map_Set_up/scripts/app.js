26a6d3bbae8beb28873101e06cb2f0e5566e3465
kikita <kikitaMoon@gmail.com>
2019-03-06 15:11:03 +0800

    Part1_Basic_Map_Set_up
    Part1_Basic_Map_Set_up

--- Part1_Basic_Map_Set_up/scripts/app.js
+++ Part1_Basic_Map_Set_up/scripts/app.js
@@ -3,8 +3,8 @@ var mapContainer = document.getElementById('map-container');
 
 // Specify the APPID & APPCODE
 var platform = new H.service.Platform({
-  app_id: 'oyNWTbKIov6XPMlrX***', // // <-- ENTER YOUR APP ID HERE
-  app_code: '_W_7qfgpOxthQ4tQQKk***', // <-- ENTER YOUR APP CODE HERE
+  app_id: 'oyNWTbKIov6XPMlrXxCH', // // <-- ENTER YOUR APP ID HERE
+  app_code: '_W_7qfgpOxthQ4tQQKkbLA', // <-- ENTER YOUR APP CODE HERE
 });
 
 
